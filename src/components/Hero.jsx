44f3081fce7a26110d493fe03093001837445d1e
mostafaguellil <113138320+mostafaguellil@users.noreply.github.com>
2024-12-27 22:41:09 +0000

    Commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,16 +17,45 @@ const Hero = () => {
             reliable, scalable systems.
           </p>
           <div className="flex justify-center lg:justify-start space-x-5">
-            <button class="flex w-full items-center justify-center gap-1 rounded-2xl bg-[#0f172a] p-5 py-3 font-semibold text-white hover:bg-rose-700">
-              Follow
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-              </svg>
-            </button>
-            <button class="text-white flex w-full items-center justify-center gap-2 rounded-2xl p-5 py-3 font-semibold bg-[#0f172a] hover:bg-green-800">
+            <a
+              href="https://www.linkedin.com/in/mostafaguellil/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full"
+            >
+              <button className="flex w-full items-center justify-center gap-1 rounded-2xl bg-[#0f172a] p-5 py-3 font-semibold text-white hover:bg-rose-700">
+                Follow
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                  />
+                </svg>
+              </button>
+            </a>
+            <button className="text-white flex w-full items-center justify-center gap-2 rounded-2xl p-5 py-3 font-semibold bg-[#0f172a] hover:bg-green-800">
               Resume
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
               </svg>
             </button>
           </div>
