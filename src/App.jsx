1241954a13386374b259b4579d59ee0cadd8c87e
mostafaguellil <113138320+mostafaguellil@users.noreply.github.com>
2025-01-01 13:59:20 +0000

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
